698798340efe2b12f7994d1fad25088dd426ae45
marcoow <marco.otte-witte@simplabs.com>
2008-11-06 20:09:10 +0100

    added install.rb; updated README

---
+++ install.rb
@@ -0,0 +1,6 @@
+puts "Copying highlight.css to #{RAILS_ROOT}/public/stylesheets/"
+
+File.copy(
+  File.join(File.dirname(__FILE__), 'assets', 'stylesheets', 'highlight.css'),
+  File.join(RAILS_ROOT, 'public', 'stylesheets')
+)

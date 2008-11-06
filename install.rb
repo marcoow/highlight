581220ed83eec89d21ca6206335536a6044da9c8
marcoow <marco.otte-witte@simplabs.com>
2008-11-06 20:16:21 +0100

    debugging

--- install.rb
+++ install.rb
@@ -1,8 +1,10 @@
 puts "Copying highlight.css to #{RAILS_ROOT}/public/stylesheets/"
-
+begin
 File.copy(
   File.join(File.dirname(__FILE__), 'assets', 'stylesheets', 'highlight.css'),
   File.join(RAILS_ROOT, 'public', 'stylesheets')
 )
-
+rescue => e
+  puts e.inspect
+end
 puts IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))

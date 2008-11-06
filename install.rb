37c36abd23d67efdf98277486931534d0e123d43
marcoow <marco.otte-witte@simplabs.com>
2008-11-06 20:15:37 +0100

    debugging

--- install.rb
+++ install.rb
@@ -4,3 +4,5 @@ File.copy(
   File.join(File.dirname(__FILE__), 'assets', 'stylesheets', 'highlight.css'),
   File.join(RAILS_ROOT, 'public', 'stylesheets')
 )
+
+puts IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
